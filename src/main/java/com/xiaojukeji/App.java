5b81bcdd995b2a9d28201ce17bc441b080c2c363
peterzhangcheng <peterzhangcheng@didichuxing.com>
2019-04-08 19:14:57 +0800

    加入log4j2日志案例

--- src/main/java/com/xiaojukeji/App.java
+++ src/main/java/com/xiaojukeji/App.java
@@ -9,7 +9,8 @@ import org.apache.logging.log4j.Logger;
 
 public class App
 {
-    static Logger logger = LogManager.getLogger(App.class.getName());
+    static Logger logger = LogManager.getLogger(App.class.getName()); //
+//    static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         String mysqlUser =  MavenProfileTest.getMysqlUser();
@@ -17,7 +18,6 @@ public class App
         logger.info("你使用的MySQDriver是：{},你使用的MySQLuser是：{}",mysqlDriver,mysqlUser);
         JSONObject jsonObject = JSON.parseObject("{\"某公司CEO\":\"东哥\",\"结婚\":\"某妹\",\"第三者\":\"某学生\"}");
         System.out.println(String.format("解析的jsonObject是:%s,\n 某公司CEO%s和%s结婚,但又跟%s发生了不正当关系",jsonObject,jsonObject.get("某公司CEO"),jsonObject.get("结婚"),jsonObject.get("第三者")));
-
         logger.info("解析的jsonObject是:{},\n 某公司CEO{}和{}结婚,但又跟{}发生了不正当关系",jsonObject,jsonObject.get("某公司CEO"),jsonObject.get("结婚"),jsonObject.get("第三者"));
     }
 }
--- src/main/java/com/xiaojukeji/MavenProfileTest.java
+++ src/main/java/com/xiaojukeji/MavenProfileTest.java
@@ -1,5 +1,8 @@
 package com.xiaojukeji;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static com.xiaojukeji.PropertyUtils.MYSQL_JDBC_DRIVER;
 import static com.xiaojukeji.PropertyUtils.MYSQL_JDBC_USER;
 
@@ -11,11 +14,18 @@ import static com.xiaojukeji.PropertyUtils.MYSQL_JDBC_USER;
  * @Version 1.0
  */
 public class MavenProfileTest {
+
+    //这里以改类全路径名称为日志器的名字，在log4j2.xml中，可以指定该日志器的日志输出
+    static Logger logger = LogManager.getLogger(MavenProfileTest.class.getName()); //
     public static String getMysqlUser(){
-        return PropertyUtils.getPropertyValue(MYSQL_JDBC_USER).toString();
+      String  mysqlUser =  PropertyUtils.getPropertyValue(MYSQL_JDBC_USER).toString();
+        logger.info("你使用的MySQLuser是：{},你使用的Logger是：{}",mysqlUser,logger.getName());
+        return mysqlUser;
     }
 
     public static String getMysqlDriver(){
-        return PropertyUtils.getBasePropertyValue(MYSQL_JDBC_DRIVER).toString();
+        String  mysqlDriver =  PropertyUtils.getBasePropertyValue(MYSQL_JDBC_DRIVER).toString();
+        logger.info("你使用的MySQDriver是：{},你使用的Logger是：{}",mysqlDriver,logger.getName());
+        return mysqlDriver;
     }
 }
